d565e20d353c53c1dd4322d79a08f20e4efcb9fb
yoyodyne1962 <112340765+yoyodyne1962@users.noreply.github.com>
2025-02-13 18:49:27 -0500

    Add files via upload
    Indexer code from professor completed in class.

--- search_engine.py
+++ search_engine.py
@@ -24,39 +24,69 @@ class Indexer:
         #     data structures for the following elements. The following
         #     are just example placeholders and you can change them as you
         #     see fit.
-        # self.tok2idx = None                       # map (token to id)
-        # self.idx2tok = None                       # map (id to token)
-        # self.postings_lists = None                # postings for each word
-        # self.docs = []                            # encoded document list
-        # self.raw_ds = None                        # raw documents for result presentation
-        # self.corpus_stats = { 'avgdl': 0 }        # any corpus-level statistics
-        # self.stopwords = stopwords.words('english')
+         self.tok2idx = {}                       # map (token to id)
+         self.idx2tok = []                       # map (id to token)
+         self.postings_lists = {}                # postings for each word
+         self.docs = []                            # encoded document list
+         self.raw_ds = None                        # raw documents for result presentation
+         self.corpus_stats = { 'avgdl': 0 }        # any corpus-level statistics
+         self.stopwords = stopwords.words('english')
 
-        if os.path.exists(dbfile):
+         if os.path.exists(dbfile):
             # TODO. If these exists a saved corpus index file, load it.
             # (You may use pickle to save and load a python object.)
-            pass
-        else:
+           with open(self.dbfile, 'rb') as f:
+                self.tok2idx,self.idx2tok,self.postings_lists,self.docs = pickle.load(f)
+            
+         else:
             # TODO. Load CNN/DailyMail dataset, preprocess and create postings lists.
             ds = load_dataset("cnn_dailymail", '3.0.0', split="test")
             self.raw_ds = ds['article']
             self.clean_text(self.raw_ds)
             self.create_postings_lists()
+            pass
 
     def clean_text(self, lst_text, query=False):
         # TODO. this function will run in two modes: indexing and query mode.
         # TODO. run simple whitespace-based tokenizer (e.g., RegexpTokenizer)
         # TODO. run lemmatizer (e.g., WordNetLemmatizer)
         # TODO. read documents one by one and process
-        for l in lst_text:
-            # TODO. complete this part
-            pass
+        if query == True:
+            return [self.tok2idx[word] for word in lemmatized]
+        else:
+            lst_text = self.raw_ds
+            for l in tqdm(lst_text):
+                            # TODO. complete this part
+                            tokenized = RegexpTokenizer(r'\w+').tokenize(l.lower())
+                            lemmatized = [WordNetLemmatizer().lemmatize(word) for word in tokenized]
+                            for word in lemmatized:
+                                if word not in self.tok2idx:
+                                    self.tok2idx[word] = len(self.tok2idx)
+                                    self.idx2tok.append(word)
 
-    # def create_postings_lists(self):
+                            self.docs.append([self.tok2idx[word] for word in lemmatized])
+
+            
+
+    def create_postings_lists(self):
         # TODO. This creates postings lists of your corpus
         # TODO. While indexing compute avgdl and document frequencies of your vocabulary
         # TODO. Save it, so you don't need to do this again in the next runs.
         # Save
+        for document in enumerate(self.docs):
+             for word_index in document:
+                    if(word_index  in self.postings_lists):
+                       self.postings_lists[word_index][document]=self.postings_lists[word_index].get(document,0)+1
+                    else:
+                        self.postings_lists[word_index]={document:1}
+            
+                             
+        with open(self.dbfile, 'wb') as f:
+            pickle.dump(self.tok2idx,self.idx2tok,self.postings_lists,self.docs,)
+
+
+
+                  
 
 
 class SearchAgent:
